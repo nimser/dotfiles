418405c295f66762cb3e83005173c7487380af5c
Nicolas Wormser <nicolas.wormser@gmail.com>
2016-10-13 19:36:43 +0400

    Fix PATH being contatenated at each session

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,4 @@
-set -U fish_user_paths $fish_user_paths ~/.rbenv/bin
+set -x fish_user_paths $fish_user_paths ~/.rbenv/bin
 set -x EDITOR 'nvim'
 status --is-interactive; and . (rbenv init -|psub)
 
