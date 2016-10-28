739082b3f6389082fbcfc8be8b500d71ef05489b
Nicolas Wormser <nicolas.wormser@gmail.com>
2016-10-28 20:14:53 +0400

    Add ~/bin folder to PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 set -gx fish_user_paths $fish_user_paths ~/.rbenv/bin
 set -gx fish_user_paths $fish_user_paths ~/.node_modules/bin
+set -gx fish_user_paths $fish_user_paths ~/bin/
 set -gx EDITOR 'nvim'
 status --is-interactive; and . (rbenv init -|psub)
 
