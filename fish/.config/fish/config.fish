dfd4c3cbc448bb07eb0c0a0b21e8de7e5ad6845b
Nicolas Wormser <nicolas.wormser@gmail.com>
2017-04-20 15:18:27 +0800

    Disable base16-specific confic causing issues with Spacemacs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,9 +4,3 @@ set -gx fish_user_paths $fish_user_paths ~/.local/bin
 set -gx fish_user_paths $fish_user_paths ~/bin/
 set -gx EDITOR 'nvim'
 status --is-interactive; and . (rbenv init -|psub)
-
-# Base16 Shell
-if status --is-interactive
-    eval (sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh)
-end
-
