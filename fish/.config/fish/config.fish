e0c113c747239a5f742e6e8384474c151d267b98
Nicolas Wormser <nicolas.wormser@gmail.com>
2017-04-29 17:04:06 +0800

    Set gpg-agent as the SSH agent

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,4 +3,10 @@ set -gx fish_user_paths $fish_user_paths ~/.node_modules/bin
 set -gx fish_user_paths $fish_user_paths ~/.local/bin
 set -gx fish_user_paths $fish_user_paths ~/bin/
 set -gx EDITOR 'nvim'
+
+## Using gpg-agent as the SSH agent
+gpgconf --launch gpg-agent
+set -e SSH_AUTH_SOCK
+set -U -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
+
 status --is-interactive; and . (rbenv init -|psub)
