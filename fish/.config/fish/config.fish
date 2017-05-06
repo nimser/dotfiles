6b56744551afc8464ab1f0dc423368a79ddcdf29
Nicolas Wormser <nicolas.wormser@gmail.com>
2017-05-06 22:50:48 +0800

    Add java-jdk memory options

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,4 +3,5 @@ set -gx fish_user_paths $fish_user_paths ~/.node_modules/bin
 set -gx fish_user_paths $fish_user_paths ~/.local/bin
 set -gx fish_user_paths $fish_user_paths ~/bin/
 set -gx EDITOR 'nvim'
+set -gx JAVA_OPTS "-Xms512m -Xmx512m"
 status --is-interactive; and . (rbenv init -|psub)
