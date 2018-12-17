80e43f316b4c7b8b3afd9213562ef75f807e8a68
Nicolas Wormser <nicolas.wormser@gmail.com>
2018-12-18 00:20:45 +0800

    Only install packages when needed

--- _system/scripts/step2_system_extras.sh
+++ _system/scripts/step2_system_extras.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
+if [[ -f `pwd`/sharedfuncs ]]; then
+  source sharedfuncs
+else
+  echo "missing file: sharedfuncs"
+  exit 1
+fi
+
 sudo pacman -Syu
 
 # SETTING DESCENT MIRRORS
-yay -S --noconfirm reflector
+aur_package_install "reflector"
 sudo reflector -n 10 --save /etc/pacman.d/mirrorlist
 ##
 # INSTALL
 #
 echo "Installing essentials...\n"
 
-sudo pacman -S --noconfirm i3-wm i3blocks i3lock dmenu nodm # window manager
-sudo pacman -S --noconfirm alacritty fish
-sudo pacman -S --noconfirm neovim xsel ack
-sudo pacman -S --noconfirm emacs
-sudo pacman -S --noconfirm stow
+package_install "i3-wm i3blocks i3lock dmenu nodm" # window manager
+package_install "alacritty fish"
+package_install "neovim xsel ack"
+package_install "emacs"
+package_install "stow"
 # NOTE: FiraCode would be grate but no ligature support in alacritty for now. See https://github.com/jwilm/alacritty/issues/50
 # NOTE: adobe-source-code-pro-fonts used but not required by emacs/spacemacs
-sudo pacman -S --noconfirm ttf-hack noto-fonts-cjk adobe-source-code-pro-fonts
-sudo pacman -S --noconfirm maim # screenshots / screencasting
-sudo pacman -S --noconfirm yarn
-sudo pacman -S --noconfirm chromium firefox
+package_install "ttf-hack noto-fonts-cjk adobe-source-code-pro-fonts"
+package_install "maim" # screenshots / screencasting
+package_install "yarn"
+package_install "chromium firefox"
 
 # PERFORMANCE IMPROVMENTS
-yay -S --noconfirm zramswap
+aur_package_install "zramswap"
 sudo systemctl start zramswap.service
 sudo systemctl enable zramswap.service
-yay -S --noconfirm profile-sync-daemon # syncs browser profiles to ram
+aur_package_install "profile-sync-daemon" # syncs browser profiles to ram
 systemctl --user start psd.service
 systemctl --user enable psd.service
 # chromium has the `disk-cache-dir` flag to set  where to store cache (see ~/.config/chromium-flags.conf) but firefox needs manual setup in about:config. A better alternative is to set the whole ~/.cache directory as a tmpfs as below
@@ -62,15 +69,16 @@ stow -R spacemacs
 stow -R xinit
 
 echo "Installing and configuring NVM...\n"
-yay -S --noconfirm nvm
+aur_package_install "nvm"
 yarn config set prefix ~/.local/
 
 echo "TODO - Installing and configuring Rbenv...\n"
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 bash -c "cd ~/.rbenv && src/configure && make -C src"
-rbenv install $UD_RUBY_VERSION # rbenv intall -l to see all versions
-rbenv global $UD_RUBY_VERSION
+rbenv install $UD_RUBY_VERSION # rbenv intall -l to see all versions FIXME: rbenv not in PATH
+rbenv global $UD_RUBY_VERSION # FIXME: rbenv not in PATH
+
 
 echo "Installing and configuring Elm..."
 yarn global install elm
@@ -78,4 +86,4 @@ yarn global install elm
 
 echo "Installing fish...\n"
 curl -L https://get.oh-my.fish | fish
-omf install
+fish -c "omf install"
