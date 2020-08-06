a7855f84c4a25efa1bc2367e27d4b35bede9372f
Nicolas Wormser <nicolas.wormser@gmail.com>
2020-08-06 17:42:04 +0200

    Update alacritty typora fish configs

--- fish/.config/omf/env.fish
+++ fish/.config/omf/env.fish
@@ -4,3 +4,7 @@ set -gx EDITOR "nvim"
 set -gx DIFFPROG "nvim -d"
 set -gx JAVA_OPTS "-Xms512m -Xmx512m"
 set -gx UD_RUBY_VERSION "2.5.0"
+set -gx GTK_IM_MODULE "xim"
+set -gx XMODIFIERS "@im=ibus"
+set -gx QT_IM_MODULE "xim"
+set -gx NIX_IGNORE_SYMLINK_STORE 1
