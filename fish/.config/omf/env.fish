f9ea28227c9327a39d188cd0419f0d42b428ae9b
Nicolas Wormser <nicolas.wormser@gmail.com>
2022-07-06 17:15:32 +0800

    Fix fcitx5 compose key issue

--- fish/.config/omf/env.fish
+++ fish/.config/omf/env.fish
@@ -4,9 +4,9 @@ set -gx EDITOR "nvim"
 set -gx DIFFPROG "nvim -d"
 set -gx JAVA_OPTS "-Xms512m -Xmx512m"
 set -gx UD_RUBY_VERSION "2.5.0"
-set -gx GTK_IM_MODULE "fcitx"
-set -gx XMODIFIERS "@im=fcitx"
-set -gx QT_IM_MODULE "fcitx"
+set -gx GTK_IM_MODULE "fcitx5"
+set -gx XMODIFIERS "@im=fcitx5"
+set -gx QT_IM_MODULE "fcitx5"
 set -gx NIX_IGNORE_SYMLINK_STORE 1
 set -gx GOLDENDICT_DIR /home/owner/local/goldendict/lib
 set -gx TESSDATA_PREFIX /home/owner/local/goldendict/ocr
