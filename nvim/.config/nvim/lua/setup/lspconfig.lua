f6863d1f68cf7fd448cbee105c039f023cd0931b
Nicolas Wormser <nicolas.wormser@gmail.com>
2023-01-04 23:18:32 +0100

    Add astro lsp

--- nvim/.config/nvim/lua/setup/lspconfig.lua
+++ nvim/.config/nvim/lua/setup/lspconfig.lua
@@ -8,20 +8,29 @@ local on_attach = function(_, bufnr)
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
   vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, bufopts)
+  -- TODO for format_on_save, see inspiration here: https://youtu.be/ajmK0ZNcM4Q?t=2314
   -- TODO better LSP rename using quickfix: https://www.reddit.com/r/neovim/comments/tjzmnt/better_lsp_rename/
   -- FIXME vim.keymap.set("n", "<Leader>lf", vim.lsp.buf.format, {async=true,buffer=0})
   -- FIXME gd doesn't work properly for lua requires
 end
 local has_cmp, cmp_lsp = pcall(require, 'cmp_nvim_lsp')
-require('lspconfig')['tsserver'].setup{
+require('lspconfig').tsserver.setup{
   capabilities = has_cmp and cmp_lsp.default_capabilities() or nil,
   on_attach = on_attach,
 }
-require('lspconfig')['svelte'].setup{
+require('lspconfig').tailwindcss.setup{
   capabilities = has_cmp and cmp_lsp.default_capabilities() or nil,
   on_attach = on_attach,
 }
-require('lspconfig')['sumneko_lua'].setup{
+require('lspconfig').svelte.setup{
+  capabilities = has_cmp and cmp_lsp.default_capabilities() or nil,
+  on_attach = on_attach,
+}
+require('lspconfig').astro.setup{
+  capabilities = has_cmp and cmp_lsp.default_capabilities() or nil,
+  on_attach = on_attach,
+}
+require('lspconfig').sumneko_lua.setup{
   capabilities = has_cmp and cmp_lsp.default_capabilities() or nil,
   on_attach = on_attach,
   settings = {
