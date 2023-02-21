ebca787e575eb3c8c664158011c2309dc9ef67e4
Nicolas Wormser <nicolas@codewithflair.org>
2023-02-21 12:36:09 +0100

    Update .gitconfig / abbrevs

--- fish/.config/fish/conf.d/abbrevs.fish
+++ fish/.config/fish/conf.d/abbrevs.fish
@@ -5,15 +5,19 @@ abbr --add vim nvim -p
 abbr --add dbxex --set-cursor "distrobox-export --bin /usr/sbin/% --export-path ~/.local/distrobox/bin"
 # Git
 abbr --add gst git status
+abbr --add gita git add
+abbr --add gaa git add -A
 abbr --add gci git commit
 abbr --add gcim --set-cursor "git commit -m '%'"
-abbr --add gaa git add -A
-abbr --add gita git add
 abbr --add gaci --set-cursor "git add -A;git commit -m '%'"
 abbr --add gcia git commit --amend --no-edit
 abbr --add gciam --set-cursor "git commit --amend -m '%'"
+abbr --add gpo git push origin
+abbr --add glg git lg
 abbr --add gco git checkout
 abbr --add gbr git branch
+# Shortcuts
+abbr --add eabbr nvim -p ~/.config/fish/conf.d/abbrevs.fish  
 # Display / output
 abbr --add xrt --set-cursor "xrandr --output HDMI-1% --above eDP-1 --primary"
 abbr --add xrb --set-cursor "xrandr --output HDMI-1% --below eDP-1 --primary"
