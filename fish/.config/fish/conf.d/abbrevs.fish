fc3127cc833ad408f8ee9a09546bf4d9ff152e78
Nicolas Wormser <nicolas@codewithflair.org>
2023-07-10 23:45:46 +0200

    misc git abbr & i3 tweaks

--- fish/.config/fish/conf.d/abbrevs.fish
+++ fish/.config/fish/conf.d/abbrevs.fish
@@ -4,7 +4,9 @@ abbr --add reboot sudo shutdown -r now
 abbr --add pn pnpm
 abbr --add pni pnpm install
 abbr --add pnd pnpm run dev 
-abbr --add pnr --set-cursor "pnpm create vite % --template react"
+abbr --add pnt pnpm run test 
+abbr --add pcr --set-cursor "pnpm create vite % --template react"
+abbr --add pcv --set-cursor "pnpm create vite %"
 abbr --add vim nvim -p
 # Distrobox
 abbr --add dbxex --set-cursor "distrobox-export --bin /usr/sbin/% --export-path ~/.local/distrobox/bin"
@@ -15,13 +17,14 @@ abbr --add gaa git add -A
 abbr --add gci --set-cursor "git commit -m '%'"
 abbr --add gaci --set-cursor "git add -A;git commit -m '%'"
 abbr --add gcia --set-cursor "git commit --amend -m '%'"
-abbr --add gacia --set-cursor "gat add -A;git commit --amend -m '%'"
+abbr --add gacia --set-cursor "git add -A;git commit --amend -m '%'"
 abbr --add gciane git commit --amend --no-edit
 abbr --add gaciane "git add -A;git commit --amend --no-edit"
 abbr --add gpo git push origin
 abbr --add glg git lg
 abbr --add gco git checkout
 abbr --add gsw git switch
+abbr --add gsc --set-cursor "git switch -c '%'"
 abbr --add gbr git branch
 # Shortcuts
 abbr --add eabbr nvim -p ~/.config/fish/conf.d/abbrevs.fish  
