c3526bdb58efbe4fcbd6d29cacab7b14d4f720d9
Nicolas Wormser <nicolas@codewithflair.org>
2025-03-12 00:49:25 +0100

    nvim conform.lua conf

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -12,16 +12,19 @@ vim.g.snacks_animate = false
 -- 4. Resolving plugin conflicts (allows multiple plugins to use same keys in different filetypes).
 
 local opt = vim.opt
+-- Don't go to begining of line with a # comment
+vim.opt.cindent = true
+vim.opt.cinkeys:remove('0#')
 -- formatoptions: r->auto comments on i_<cr>, o->auto comments on n_o/n_O (n_ notation => normal mode)
 -- use i_<c-u> to clearup any unwanted auto comment
 -- opt.formatoptions:append("ro") -- lazyvim defaults: "jcroqlnt", neovim defaults: "tcqj"
 opt.cursorcolumn = true -- Highlights the current column
-opt.foldenable = false -- folds are expanded when file opens
+opt.foldenable = false  -- folds are expanded when file opens
 -- opt.lazyredraw = true -- avoids redrawing while performing macros to prevent lags
 opt.listchars = { tab = " ", trail = "·" }
-opt.scrolloff = 6 -- Lines of context
+opt.scrolloff = 6        -- Lines of context
 opt.signcolumn = "yes:1" -- always show signcolumns
-opt.title = true -- Allows neovim to send the Terminal details of the current window, instead of just getting 'v'
+opt.title = true         -- Allows neovim to send the Terminal details of the current window, instead of just getting 'v'
 opt.whichwrap = "[,]"
 opt.wrap = true
 
