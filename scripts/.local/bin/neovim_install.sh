c36895997e1e3060bcac27af036ef58f85736012
Nicolas Wormser <nicolas@codewithflair.org>
2025-03-12 00:49:06 +0100

    Extract neovim appimage to /opt

--- scripts/.local/bin/neovim_install.sh
+++ scripts/.local/bin/neovim_install.sh
@@ -5,16 +5,21 @@
 set -e
 
 NVIM_PATH="/usr/local/bin/nvim"
+NVIM_EXTRACTED_DIR="/opt/nvim"
 TEMP_DIR=$(mktemp -d)
 ARCH=$(uname -m)
 APPIMAGE_URL="https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-$ARCH.appimage"
 
 echo "Downloading latest Neovim nightly AppImage"
 curl -sL "$APPIMAGE_URL" -o "$TEMP_DIR/nvim.appimage"
-
 chmod +x "$TEMP_DIR/nvim.appimage"
-echo "Installing to $NVIM_PATH..."
-sudo mv "$TEMP_DIR/nvim.appimage" $NVIM_PATH
+
+echo "Extracting AppImage..."
+cd "$TEMP_DIR"
+sudo mkdir -p "$NVIM_EXTRACTED_DIR"
+./nvim.appimage --appimage-extract
+sudo cp -r squashfs-root/* "$NVIM_EXTRACTED_DIR/"
+sudo ln -sf "$NVIM_EXTRACTED_DIR/AppRun" "$NVIM_PATH"
 
 rm -rf "$TEMP_DIR"
 echo "Installed $(nvim --version | head -n 1)"
